87eeb9c975d0acfbd07b16e099785530140b2381
Kyle Maxwell <kyle@kylemaxwell.com>
2009-12-29 17:33:55 -0800

    better docs and passing tests

--- jquery.parsley.js
+++ jquery.parsley.js
@@ -30,16 +30,18 @@ StringNode.prototype.toString = function() {
  * This is similar to a jQuery list of HTML Nodes.  I need to be able to
  * call methods on it, and each contained String needs an associated position.
  */
-function StringNodeList() {};
+function StringNodeList() {
+  this.multiple = false;
+};
 StringNodeList.prototype = new Array();
 
 StringNodeList.prototype.toString = function() {
-  var buffer = "[";
+  var buffer = "<StringNodeList[";
   for(var i = 0; i < this.length; i++){
     if(i > 0) buffer += ", ";
     buffer += this[i].toString();
   }
-  return buffer + "]";
+  return buffer + "]>";
 }
 
 /**
@@ -47,7 +49,7 @@ StringNodeList.prototype.toString = function() {
  */
 StringNodeList.prototype.normalizeSpace = function() {
   jQuery.each(this, function() {
-    this.string = this.string.replace(/\s+/g, ' ').replace(/^\s+/m, '').replace(/\s+$/m, '');
+    this.string = pQuery.normalizeSpace(this.string);
   });  
   return this;
 }
@@ -55,7 +57,7 @@ StringNodeList.prototype.normalizeSpace = function() {
 /**
  * Creates an array of strings that mirrors this StringNodeList.
  */
-StringNodeList.prototype.toSimple = function() {
+StringNodeList.prototype.simple = function() {
   var array = [];
   jQuery.each(this, function() {
     array.push(this.string);
@@ -75,7 +77,7 @@ StringNodeList.prototype.toSimple = function() {
  * 
  * The following shortcut arguments are also available:
  * - extract() 
- *   => function(node) { return jQuery(node).text(); }
+ *   => function(node) { return pQuery.normalizeSpace(jQuery(node).text()); }
  * - extract("@foo") 
  *   => function(node) { return jQuery(node).attr("foo"); }
  * - extract(/regex/) 
@@ -84,12 +86,12 @@ StringNodeList.prototype.toSimple = function() {
 jQuery.fn.extract = function(func) {
   if(!func){
     func = function(node) {
-      return jQuery(node).text();
+      return pQuery.normalizeSpace(jQuery(node).text());
     };
   }
 
   // extract(/regex/)
-  if (func instanceof RegExp) {
+  if(func instanceof RegExp) {
     var re = func;
     func = function(node) {
       var text = jQuery(node).text();
@@ -98,7 +100,7 @@ jQuery.fn.extract = function(func) {
   }
   
   // extract("@attribute")
-  if (typeof(func) == "string" && func[0] == "@") {
+  if(typeof(func) == "string" && func[0] == "@") {
     var attr = func.substring(1);
     func = function(node) {
       return jQuery(node).attr(attr);
@@ -110,4 +112,92 @@ jQuery.fn.extract = function(func) {
     list.push(new StringNode(func(this), this.sourceIndex));
   });
   return list;
+}
+
+function pQuery(){};
+
+/**
+ * Slightly voodoo.  This function cleans up any remaining jQuery
+ * objects by running extract on them, then tries to group all of the 
+ * StringNodeLists by their implicit page ordering.  Then it simplifies
+ * the resulting data structure to vanilla object/arrays.
+ * 
+ */
+pQuery.extractAndGroup = function(parselet) {
+  pQuery.extract(parselet);
+  pQuery.group(parselet)
+  return parselet;
+}
+
+pQuery.keys = function(object) {
+  var a = [];
+  jQuery.each(object, function(key){
+    a.push(key);
+  });
+  return a;
+}
+
+pQuery.compileNodes = function(object) {
+  var nodes = [];
+  jQuery.each(object, function(key, value) {    
+    if(value instanceof StringNodeList) {
+      jQuery.each(value, function(i, node) {
+        node.multiple = value.multiple;
+        node.key = key;
+        nodes.push(node);
+      });
+    }
+  });
+  return nodes.sort(function(a,b) { return a.position - b.position; });
+}
+
+pQuery.group = function(parselet) {
+  jQuery.each(parselet, function(key, value) {    
+    // alert(key + typeof value + typeof(value[0]));
+    if(value instanceof Array && typeof(value[0]) == "object") {
+      // Drop all nodes into a huge array, then iterate, grouping as we go
+      var allNodes = pQuery.compileNodes(value[0]);
+      var node;
+      var groups = [];
+      var group = {};
+      groups.push(group);
+      while(node = allNodes.shift()) {
+        if(!node.multiple && group[node.key]){
+          group = {};
+          groups.push(group)
+        }
+        if(node.multiple){
+          if(!group[node.key]) group[node.key] = [];
+          group[node.key].push(node.string);
+        } else {
+          group[node.key] = node.string;
+        }
+      }
+      parselet[key] = groups;
+    } else if(typeof(value) == "object" && !(value instanceof StringNodeList)) {
+      pQuery.group(value); //recurse
+    }
+  });  
+}
+
+pQuery.normalizeSpace = function(string) {
+  return string.replace(/\s+/g, ' ').replace(/^\s+/m, '').replace(/\s+$/m, '');
+}
+
+pQuery.extract = function(parselet) {
+  jQuery.each(parselet, function(key, value) {
+    if(typeof(value) == "array") { 
+      if(value[0] instanceof StringNodeList) {
+        parselet[key] = value = value[0].multiple();
+      } else {
+        pQuery.extract(value);
+      }
+    } else if(value instanceof jQuery) {
+      parselet[key] = value = value.extract();
+    } else if(value instanceof StringNodeList) {
+      //Nothing
+    } else if(typeof(value) == "object") {
+      pQuery.extract(value);
+    }
+  });
 }
